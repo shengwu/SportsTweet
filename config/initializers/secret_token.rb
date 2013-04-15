b9f24989e13bd8feee20eed2aac1ca7d6e4fc470
Sheng Wu <pkernoobie@gmail.com>
2013-04-14 20:41:16 -0500

    Add rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsTweet::Application.config.secret_token = '3569e25aa41467f65811a2b77971ba3c8559b685035c387dbffdc1725994e8190ffa2d2ba0406c1e759d7ff6121895c659ae397f6a4c5f5a912676ecba64bdc3'
