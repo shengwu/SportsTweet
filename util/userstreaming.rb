1810b1c49d39a188caaf66fbea09ed0785a71e27
rc <rc@debian.northwestern.edu>
2013-05-10 16:20:32 -0500

    if we decide to do userstreaming...

---
+++ util/userstreaming.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'tweetstream'
+
+TweetStream.configure do |config|
+  config.consumer_key       = ENV['CONSUMER_KEY']
+  config.consumer_secret    = ENV['CONSUMER_SECRET']
+  config.oauth_token        = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_TOKEN_SECRET']
+  config.auth_method        = :oauth
+end
+
+client = TweetStream::Client.new
+
+client.userstream do |status|
+  puts status.text
+end
