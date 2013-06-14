86540713dd0f5daf3a148d38b30ce7d119a08d57
Sheng Wu <pkernoobie@gmail.com>
2013-06-14 10:30:14 -0500

    Increment mentions, change bar colors

--- util/tweets.rb
+++ util/tweets.rb
@@ -24,19 +24,19 @@ TweetStream::Client.new.track('nba') do |tweet|
   # Only accept English language tweets
   if tweet.lang == "en"
     # Check for mentions of top teams and players in tweet
-    top_teams = CachedResult.where(:name => "top_teams")[0].result
-    top_players = CachedResult.where(:name => "top_players")[0].result
+    top_teams = CachedResult.where(:name => "top_teams")[0].result.map{|team| team[0]}
+    top_players = CachedResult.where(:name => "top_players")[0].result.map{|player| player[0]}
     mentions = Hash.new(0)
     top_teams.each do |team|
       fragments = team.downcase.split
       city = fragments[0..-2].join(' ')
       name = fragments.last
-      if tweet.downcase.include? city or tweet.downcase.include? name
+      if tweet.text.downcase.include? city or tweet.text.downcase.include? name
         mentions[team] += 1
       end
     end
     top_players.each do |player|
-      if tweet.text.downcase.include?(player.downcase)
+      if tweet.text.downcase.include? player.downcase
         mentions[player] += 1
       end
     end
